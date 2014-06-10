59a7fc1b737b2188aa222db179703b9a1c8665f9
alvare <welcometothechango@gmail.com>
2014-06-10 13:00:47 -0300

    much applicative ! very <*> wow

--- Data/DSON/Parsec.hs
+++ Data/DSON/Parsec.hs
@@ -1,29 +1,28 @@
+module Data.DOSN.Parsec (
+    Dson(..),
+    dsonTop
+    ) where
+
 import Control.Applicative
 import Control.Monad
 import Text.Parsec hiding (Empty, many, optional, (<|>))
 import Text.Parsec.String (Parser)
-import Text.Parsec.Language (javaStyle)
+import Text.Parsec.Language (emptyDef)
 import qualified Text.Parsec.Token as P
 
-type Dict = [(String, Dson)]
-
-data Dson = Dict Dict | Array [Dson] | String String | Number Double | Yes | No | Empty
+data Dson = DSDict [(String, Dson)] | DSArray [Dson] | DSString String | DSNumber Double | Yes | No | Empty
     deriving (Show, Eq)
 
-lexer = P.makeTokenParser javaStyle
+lexer = P.makeTokenParser emptyDef
 symbol = P.symbol lexer
 stringLiteral = P.stringLiteral lexer
 naturalOrFloat = P.naturalOrFloat lexer
 
-anyNotBackslashComilla :: Parser Char
-anyNotBackslashComilla = noneOf "\""
-
-
 dsonString :: Parser Dson
-dsonString = fmap String stringLiteral
+dsonString = fmap DSString stringLiteral
 
 dsonInt :: Parser Dson
-dsonInt = liftM (either (Number . fromInteger) Number) naturalOrFloat
+dsonInt = pure (DSNumber . (either fromInteger id)) <*> naturalOrFloat
 
 dsonBool :: Parser Dson
 dsonBool = try (string "yes" >> return Yes) <|>
@@ -38,24 +37,13 @@ dsonArray = do
     symbol "so"
     vals <- sepBy dsonValue dsonArraySep
     symbol "many"
-    return $ Array vals
+    return $ DSArray vals
   where dsonArraySep = try (symbol "also") <|> (symbol "and")
 
 dsonDict :: Parser Dson
-dsonDict = do
-    symbol "such"
-    vals <- keyValue `sepBy` dsonDictSep
-    symbol "wow"
-    return $ Dict vals
-  where dsonDictSep = oneOf ".,!?"
-        dsonKey = stringLiteral
-        keyValue = do
-            key <- dsonKey
-            symbol "is"
-            val <- dsonValue
-            return (key, val)
-
-dsonDIct2 :: Parser [(String, Dson)]
-dsonDIct2 = symbol "such" *> (kvPairs `sepBy` dsonSep) <* symbol "wow"
+dsonDict = DSDict <$> (symbol "such" *> (kvPairs `sepBy` dsonSep) <* symbol "wow")
   where kvPairs = (,) <$> stringLiteral <*> (symbol "is" *> dsonValue)
-        dsonSep = spaces *> oneOf ".,!?" <* spaces
+        dsonSep = oneOf ".,!?" <* spaces
+
+dsonTop :: Parser Dson
+dsonTop = try dsonDict <|> dsonArray

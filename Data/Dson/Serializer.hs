af8b3e68f584ffd13f45681f99425e3f84f3ee1e
alvare <welcometothechango@gmail.com>
2014-06-11 15:01:00 -0300

    wrap string in comass

--- Data/Dson/Serializer.hs
+++ Data/Dson/Serializer.hs
@@ -6,12 +6,14 @@ import Data.List
 
 dsonToJson :: Dson -> String
 dsonToJson (DSDict list) = "{ " ++ (concat . (intersperse ", ") . (map showDict)) list ++ " }"
-    where showDict (key, val) = key ++ ": " ++ (dsonToJson val)
+    where showDict (key, val) = (wrapLiteral key) ++ ": " ++ (dsonToJson val)
 
 dsonToJson (DSArray list) = "[" ++ (concat . (intersperse ", ") . (map dsonToJson)) list ++ "]"
 
-dsonToJson (DSString str) = str
+dsonToJson (DSString str) = wrapLiteral str
 dsonToJson (DSNumber n) = show n
 dsonToJson Yes = "true"
 dsonToJson No = "false"
 dsonToJson Empty = "null"
+
+wrapLiteral s = "\"" ++ s ++ "\""

5f5ddd967ca6f9711903e2e51d27b00162161439
alvare <welcometothechango@gmail.com>
2014-06-17 15:10:29 -0300

    cleaner main

--- Data/Dson.hs
+++ Data/Dson.hs
@@ -1,20 +1,14 @@
 module Data.Dson where
 
 import Text.Parsec
-
-import Data.Dson.Parsec
 import Text.Parsec.String (Parser)
+import Data.Dson.Parsec
 import Data.Dson.Serializer (dsonToJson)
 
 run :: Parser Dson -> String -> IO ()
-run p input
-        = case (parse p "" input) of
-            Left err -> do
-                           putStr "parse error at "
-                           print err
-            Right x  -> print $ dsonToJson x
+run p input = case (parse p "" input) of
+                  Left err -> putStr "parse error at " >> print err
+                  Right x  -> print $ dsonToJson x
 
 main :: IO ()
-main = do
-         args <- getLine
-         run dsonTop args
+main = run dsonTop =<< getLine
